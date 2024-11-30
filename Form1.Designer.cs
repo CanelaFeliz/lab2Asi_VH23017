dfc8bf91a6fbe9415553ab0e81e8ed6d6368be06
Galleta <galletadecanela2048@gmail.com>
2024-11-29 18:05:15 -0600

    Laboratorio 2

---
+++ Form1.Designer.cs
@@ -0,0 +1,388 @@
+﻿
+namespace lab2Asi_VH23017
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbxServicio = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtMascota = new System.Windows.Forms.TextBox();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.txtCantAdultas = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtCantJovenes = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtServicio = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtTotalMes = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(353, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "My Beauty Pet";
+            // 
+            // cbxServicio
+            // 
+            this.cbxServicio.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxServicio.FormattingEnabled = true;
+            this.cbxServicio.Items.AddRange(new object[] {
+            "Baño $7",
+            "Corte $15",
+            "Servicio completo $15"});
+            this.cbxServicio.Location = new System.Drawing.Point(164, 83);
+            this.cbxServicio.Name = "cbxServicio";
+            this.cbxServicio.Size = new System.Drawing.Size(121, 21);
+            this.cbxServicio.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cbxServicio);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtEdad);
+            this.groupBox1.Controls.Add(this.txtMascota);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Location = new System.Drawing.Point(62, 91);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(326, 206);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // txtMascota
+            // 
+            this.txtMascota.Location = new System.Drawing.Point(164, 36);
+            this.txtMascota.Name = "txtMascota";
+            this.txtMascota.Size = new System.Drawing.Size(121, 20);
+            this.txtMascota.TabIndex = 3;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(372, 353);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 4;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Location = new System.Drawing.Point(477, 353);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(75, 23);
+            this.btnCerrar.TabIndex = 4;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(272, 353);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 4;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(68, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Nombre mascota";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(68, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Servicio";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(68, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Edad";
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Location = new System.Drawing.Point(164, 130);
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(46, 20);
+            this.txtEdad.TabIndex = 3;
+            // 
+            // txtCantAdultas
+            // 
+            this.txtCantAdultas.Enabled = false;
+            this.txtCantAdultas.Location = new System.Drawing.Point(169, 31);
+            this.txtCantAdultas.Name = "txtCantAdultas";
+            this.txtCantAdultas.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txtCantAdultas.Size = new System.Drawing.Size(121, 20);
+            this.txtCantAdultas.TabIndex = 3;
+            this.txtCantAdultas.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(44, 31);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Cantidad adultas";
+            // 
+            // txtCantJovenes
+            // 
+            this.txtCantJovenes.Enabled = false;
+            this.txtCantJovenes.Location = new System.Drawing.Point(169, 76);
+            this.txtCantJovenes.Name = "txtCantJovenes";
+            this.txtCantJovenes.Size = new System.Drawing.Size(121, 20);
+            this.txtCantJovenes.TabIndex = 3;
+            this.txtCantJovenes.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(44, 75);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(89, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Cantidad jovenes";
+            // 
+            // txtServicio
+            // 
+            this.txtServicio.Enabled = false;
+            this.txtServicio.Location = new System.Drawing.Point(169, 122);
+            this.txtServicio.Name = "txtServicio";
+            this.txtServicio.Size = new System.Drawing.Size(121, 20);
+            this.txtServicio.TabIndex = 3;
+            this.txtServicio.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(44, 121);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(111, 13);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Servicio mas brindado";
+            // 
+            // txtTotalMes
+            // 
+            this.txtTotalMes.Enabled = false;
+            this.txtTotalMes.Location = new System.Drawing.Point(169, 165);
+            this.txtTotalMes.Name = "txtTotalMes";
+            this.txtTotalMes.Size = new System.Drawing.Size(121, 20);
+            this.txtTotalMes.TabIndex = 3;
+            this.txtTotalMes.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(44, 164);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(70, 13);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Total del mes";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.comboBox1);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Controls.Add(this.label11);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Location = new System.Drawing.Point(0, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(326, 206);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Entrada";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Baño $7",
+            "Corte $15",
+            "Servicio completo $15"});
+            this.comboBox1.Location = new System.Drawing.Point(164, 83);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(68, 137);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(32, 13);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "Edad";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(68, 83);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(45, 13);
+            this.label10.TabIndex = 0;
+            this.label10.Text = "Servicio";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(68, 39);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(87, 13);
+            this.label11.TabIndex = 0;
+            this.label11.Text = "Nombre mascota";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(164, 130);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(46, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(164, 36);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(121, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Controls.Add(this.txtCantAdultas);
+            this.groupBox3.Controls.Add(this.txtCantJovenes);
+            this.groupBox3.Controls.Add(this.txtServicio);
+            this.groupBox3.Controls.Add(this.label8);
+            this.groupBox3.Controls.Add(this.txtTotalMes);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Location = new System.Drawing.Point(439, 91);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(326, 206);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Resultados";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Registro mascotas";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbxServicio;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.TextBox txtMascota;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtCantAdultas;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtCantJovenes;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtServicio;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtTotalMes;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab2Asi_VH23017
+{
+    public partial class Form1 : Form
+    {
+
+        //Cristian Mauricio Vasquez Hernandez
+        //28 - 09 - 2024
+
+        public Form1()
+        {
+            InitializeComponent();
+            cbxServicio.SelectedIndex = 0;
+        }
+
+        //Metodo de limpieza
+        private void Limpiar()
+        {
+            cbxServicio.SelectedIndex = 0;
+            txtEdad.Text = "";
+            txtMascota.Text = "";
+        }
+
+        //Boton salir
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //Boton limpiar
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+            txtCantAdultas.Text = "";
+            txtCantJovenes.Text = "";
+            txtTotalMes.Text = "";
+            txtServicio.Text = "";
+            cantAdulto = 0;
+            cantJoven = 0;
+            servBano = 0;
+            servCorte = 0;
+        }
+
+        //Variables
+        string mascota;
+        int edad = 0;
+        string servicio;
+        double precio;
+        double descuento;
+        double totalMascota;
+        int cantAdulto = 0;
+        int cantJoven = 0;
+        double totalMes;
+        int servBano = 0;
+        int servCorte = 0;
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            //validaciones
+            if (string.IsNullOrEmpty(txtMascota.Text))
+            {
+                MessageBox.Show("Ingrese un nombre");
+                txtMascota.Text = "";
+            }
+            else if (string.IsNullOrEmpty(txtEdad.Text) || !int.TryParse(txtEdad.Text, out edad))
+            {
+                MessageBox.Show("Ingrese una edad");
+                txtEdad.Text = "";
+            }
+            //edad
+            else
+            {
+                edad = Convert.ToInt32(txtEdad.Text);
+                mascota = txtMascota.Text;
+
+                //Mascota Adulta
+                if (edad > 5)
+                {
+                    descuento = 0.25;
+                    cantAdulto++;
+                }
+                //Mascota Joven
+                else
+                {
+                    descuento = 0.1;
+                    cantJoven++;
+                }
+                //servicio
+                switch (cbxServicio.SelectedIndex)
+                {
+                    //Servicio Baño
+                    case 0:
+                        servicio = "Baño";
+                        precio = 7;
+                        totalMascota = precio - (precio * descuento);
+                        totalMes += totalMascota;
+                        servBano++;
+                        MessageBox.Show($"Mascota: {mascota}\nPrecio: ${Convert.ToString(totalMascota)}\nServicio: {servicio}\nCantidad de servicios: 1");
+                        Limpiar();
+                        break;
+                    //Servicio Corte
+                    case 1:
+                        servicio = "Corte";
+                        precio = 10;
+                        totalMascota = precio - (precio * descuento);
+                        totalMes += totalMascota;
+                        servCorte++;
+                        MessageBox.Show($"Mascota: {mascota}\nPrecio: ${Convert.ToString(totalMascota)}\nServicio: {servicio}\nCantidad de servicios: 1");
+                        Limpiar();
+                        break;
+                    //Servicio Completo
+                    case 2:
+                        servicio = "Servicio Completo";
+                        precio = 17;
+                        totalMascota = precio - (precio * descuento);
+                        totalMes += totalMascota;
+                        servCorte++;
+                        servBano++;
+                        MessageBox.Show($"Mascota: {mascota}\nPrecio: ${Convert.ToString(totalMascota)}\nServicio: {servicio}\nCantidad de servicios: 2");
+                        Limpiar();
+                        break;
+                }
+
+                //Salidas
+                txtCantAdultas.Text = Convert.ToString(cantAdulto);
+                txtCantJovenes.Text = Convert.ToString(cantJoven);
+                txtTotalMes.Text = $"${Convert.ToString(totalMes)}";
+
+                //Ciclo de servicio mas ofrecido
+                if (servBano > servCorte)
+                {
+                    txtServicio.Text = "Baño";
+                }
+                else if (servCorte > servBano)
+                {
+                    txtServicio.Text = "Corte";
+                }
+                else
+                {
+                    txtServicio.Text = "Misma cantidad";
+                }
+            }
+        }
+    }
+}
+// Un saludo al inge :)
